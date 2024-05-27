1549c7a5b38a7cd70a7815f7792840972e87d930
Mohammad Nehaal <108192533+Mnehaal2000@users.noreply.github.com>
2024-05-27 15:11:19 +0500

    v1

--- src/web/components/Header.jsx
+++ src/web/components/Header.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import logo from "../../assets/Logo.png"
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../../AuthContext';
-
 const Header = () => {
     const { logout, currentUser, userType } = useAuth()
     const [openHam, setOpenHam] = useState(false);
@@ -16,16 +15,20 @@ const Header = () => {
                     <p className="flex items-center space-x-3 rtl:space-x-reverse">
                         <img src={logo} alt="Logo" />
                     </p>
-                    <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-
+                    <div className="flex md:order-2 gap-[5px] space-x-3 md:space-x-0 rtl:space-x-reverse">
                         {!currentUser ? (
-                            <Link className='w-full' to="/auth/signup">
-                                <button className="text-white flex flex-col justify-center items-center bg-[#FFA90A] outline-none font-medium rounded-lg text-sm px-4 py-2">Signup</button>
-                            </Link>
+                            <>
+                                <Link className='w-full' to="/auth/signup">
+                                    <button className="text-white flex flex-col justify-center items-center bg-[#FFA90A] outline-none font-medium rounded-lg text-sm px-4 py-2">Signup</button>
+                                </Link>
+                                <Link className='w-full' to="/auth">
+                                    <button className="text-white flex flex-col justify-center items-center bg-[#FFA90A] outline-none font-medium rounded-lg text-sm px-4 py-2">Login</button>
+                                </Link>
+                            </>
                         ) : (
                             userType === "user" ? (
                                 <Link className='w-full' to="/user_portal">
-                                    <button className="text-white flex flex-col justify-center items-center bg-[#4a9024] outline-none font-medium rounded-lg text-sm px-4 py-2">User Portal</button>
+                                    <button className="text-white flex flex-col justify-center items-center bg-[#4A9024] outline-none font-medium rounded-lg text-sm px-4 py-2">User Portal</button>
                                 </Link>
                             ) : (
                                 <Link className='w-full' to="/admin_portal">
@@ -33,15 +36,12 @@ const Header = () => {
                                 </Link>
                             )
                         )}
-
                         <button onClick={toggleHam} data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
                             </svg>
                         </button>
-
-
                     </div>
                     {(<div className={`items-center justify-between ${openHam ? 'block' : 'hidden'} w-full md:flex md:w-auto md:order-1`} id="navbar-cta">
                         <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
@@ -69,5 +69,9 @@ const Header = () => {
         </>
     )
 }
-
 export default Header
+
+
+
+
+
