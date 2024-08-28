ada95110ac7618c273c877509eb70fae0d4eff26
Zohaib Haider <zebihaider123@gmail.com>
2024-08-28 15:12:16 +0500

    added forgot password

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	useNavigate,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
 } from "react-router-dom";
 import WebLayout from "./web/Layout";
 import Homepage from "./web/pages/Homepage";
@@ -22,85 +22,87 @@ import MyAds from "./user_portal/pages/MyAds";
 import OngoingBids from "./user_portal/pages/OngoingBids";
 import EditProfile from "./user_portal/pages/EditProfile";
 import ChangePassword from "./user_portal/pages/ChangePassword";
+import ForgotPassword from "./auth/ForgotPassword";
 
 function App() {
-	return (
-		<>
-			<Router>
-				<Routes>
-					<Route path="/auth" element={<AuthLayout />}>
-						<Route index element={<Loginpage />} />
-						<Route path="signup" element={<Signuppage />} />
-						<Route path="admin" element={<AdminLogin />} />
-					</Route>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/auth" element={<AuthLayout />}>
+            <Route index element={<Loginpage />} />
+            <Route path="signup" element={<Signuppage />} />
+            <Route path="admin" element={<AdminLogin />} />
+            <Route path="forgotPassword" element={<ForgotPassword />} />
+          </Route>
 
-					<Route path="/" element={<WebLayout />}>
-						<Route index element={<Homepage />} />
-						<Route path="car-listing" element={<CarListing />} />
-						<Route path="car-details/:id" element={<CarDetails />} />
-					</Route>
+          <Route path="/" element={<WebLayout />}>
+            <Route index element={<Homepage />} />
+            <Route path="car-listing" element={<CarListing />} />
+            <Route path="car-details/:id" element={<CarDetails />} />
+          </Route>
 
-					<Route path="/admin_portal" element={<AdminLayout />}>
-						<Route index element={<Users />} />
-						<Route path="logout" element={<Logout />} />
-						<Route path="change-password" element={<ChangePassword />} />
-					</Route>
+          <Route path="/admin_portal" element={<AdminLayout />}>
+            <Route index element={<Users />} />
+            <Route path="logout" element={<Logout />} />
+            <Route path="change-password" element={<ChangePassword />} />
+          </Route>
 
-					<Route path="/user_portal" element={<UserLayout />}>
-						<Route index element={<MyAds />} />
-						<Route path="ongoing-bids" element={<OngoingBids />} />
-						<Route path="bids-won" element={<BidsWon />} />
-						<Route path="edit-profile" element={<EditProfile />} />
-						<Route path="change-password" element={<ChangePassword />} />
-						<Route path="logout" element={<Logout />} />
-					</Route>
-				</Routes>
-			</Router>
-		</>
-	);
+          <Route path="/user_portal" element={<UserLayout />}>
+            <Route index element={<MyAds />} />
+            <Route path="ongoing-bids" element={<OngoingBids />} />
+            <Route path="bids-won" element={<BidsWon />} />
+            <Route path="edit-profile" element={<EditProfile />} />
+            <Route path="change-password" element={<ChangePassword />} />
+            <Route path="logout" element={<Logout />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 function Logout() {
-	const { logout, userType } = useAuth();
-	const navigate = useNavigate();
-	return (
-		<>
-			<div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-[#1F2634] bg-opacity-75">
-				<div className="w-[600px] h-[280px] rounded-lg mt-[40px] flex flex-col gap-[20px] justify-center items-center bg-white p-6">
-					<p className="font-bold text-black text-2xl text-center">
-						Are you sure you want to logout?
-					</p>
-					<div className="w-full flex flex-row gap-4 justify-center">
-						{userType === "admin" ? (
-							<button
-								onClick={() => {
-									navigate("/admin_portal");
-								}}
-								className="bg-[#FFD700] hover:bg-[#FFC300] rounded-md w-[180px] h-[50px] font-bold text-white transition-all duration-300"
-							>
-								Cancel
-							</button>
-						) : (
-							<button
-								onClick={() => {
-									navigate("/user_portal");
-								}}
-								className="bg-[#FFD700] hover:bg-[#FFC300] rounded-md w-[180px] h-[50px] font-bold text-white transition-all duration-300"
-							>
-								Cancel
-							</button>
-						)}
-						<button
-							onClick={logout}
-							className="bg-[#059C4B] hover:bg-[#048C43] rounded-md w-[180px] h-[50px] font-bold text-white transition-all duration-300"
-						>
-							Confirm
-						</button>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  const { logout, userType } = useAuth();
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-[#1F2634] bg-opacity-75">
+        <div className="w-[600px] h-[280px] rounded-lg mt-[40px] flex flex-col gap-[20px] justify-center items-center bg-white p-6">
+          <p className="text-2xl font-bold text-center text-black">
+            Are you sure you want to logout?
+          </p>
+          <div className="flex flex-row justify-center w-full gap-4">
+            {userType === "admin" ? (
+              <button
+                onClick={() => {
+                  navigate("/admin_portal");
+                }}
+                className="bg-[#FFD700] hover:bg-[#FFC300] rounded-md w-[180px] h-[50px] font-bold text-white transition-all duration-300"
+              >
+                Cancel
+              </button>
+            ) : (
+              <button
+                onClick={() => {
+                  navigate("/user_portal");
+                }}
+                className="bg-[#FFD700] hover:bg-[#FFC300] rounded-md w-[180px] h-[50px] font-bold text-white transition-all duration-300"
+              >
+                Cancel
+              </button>
+            )}
+            <button
+              onClick={logout}
+              className="bg-[#059C4B] hover:bg-[#048C43] rounded-md w-[180px] h-[50px] font-bold text-white transition-all duration-300"
+            >
+              Confirm
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/auth/ForgotPassword.jsx
@@ -0,0 +1,136 @@
+import { sendPasswordResetEmail } from "firebase/auth";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { auth, db } from "../../db";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+
+const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const navigate = useNavigate();
+  const queryAllCollections = async () => {
+    const collections = ["admins", "users"];
+    for (let col of collections) {
+      const q = query(collection(db, col), where("email", "==", email));
+      const querySnapshot = await getDocs(q);
+      if (!querySnapshot.empty) {
+        // Get the first document (should only be one if emails are unique)
+        const userDoc = querySnapshot.docs[0];
+        const uid = userDoc.id;
+        // Now fetch the user's details using the UID
+        const userRef = doc(db, col, uid);
+        const userSnapshot = await getDoc(userRef);
+
+        if (userSnapshot.exists()) {
+          return { ...userSnapshot.data(), id: uid, collection: col };
+        }
+      }
+    }
+    return null;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Prevent multiple submissions
+    if (isSubmitting) return;
+    setIsSubmitting(true);
+
+    try {
+      let userData = await queryAllCollections();
+
+      if (!userData) {
+        toast.error("This email is not registered.");
+        return;
+      }
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Check your email account for reset instructions.");
+      setTimeout(() => {
+        navigate("/signIn");
+      }, 5000);
+    } catch (err) {
+      console.error("Error resetting password:", err);
+      if (err.code === "auth/too-many-requests") {
+        toast.error("Too many requests. Please try again later.");
+      } else if (err.code === "auth/invalid-email") {
+        toast.error("Invalid email address.");
+      } else {
+        toast.error("Error sending reset email. Please try again.");
+      }
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <>
+      <main
+        id="content"
+        role="main"
+        className="flex items-center justify-center h-screen p-6"
+      >
+        <div className="bg-white w-[30%] border-2 border-indigo-300 shadow-lg mt-7 rounded-xl dark:bg-white dark:border-gray-700">
+          <div className="p-4 sm:p-7">
+            <div className="text-center">
+              <h1 className="block text-2xl font-bold text-gray-800 dark:text-gray-800">
+                Forgot password?
+              </h1>
+              <p className="mt-2 text-sm text-gray-600 dark:text-gray-600">
+                Remember your password?
+                <Link
+                  className="font-medium text-black decoration-2 hover:underline"
+                  to={"/"}
+                >
+                  Login here
+                </Link>
+              </p>
+            </div>
+            <div className="mt-5">
+              <form onSubmit={(e) => handleSubmit(e)}>
+                <div className="grid gap-y-4">
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block mb-2 ml-1 text-sm font-bold dark:text-gray-800"
+                    >
+                      Email address
+                    </label>
+                    <div className="relative">
+                      <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="block w-full px-4 py-3 text-sm border-2 border-gray-200 rounded-md shadow-sm focus:border-[#ffe001] focus:ring-[#ffe001]"
+                        required
+                        aria-describedby="email-error"
+                      />
+                    </div>
+                  </div>
+                  <button
+                    type="submit"
+                    className="inline-flex items-center justify-center gap-2 px-4 py-3 text-sm font-semibold text-white transition-all bg-[#ffe001] border border-transparent rounded-md hover:bg-[#ffe001] focus:outline-none focus:ring-2 focus:ring-[#ffe001] focus:ring-offset-2 dark:focus:ring-offset-gray-800"
+                  >
+                    Reset password
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </main>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default ForgotPassword;
